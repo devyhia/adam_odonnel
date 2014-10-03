5854795e006322b474114a361f1e8b64c873d593
Yehia Abo El-Nga <yehia@devrok.com>
2014-10-03 00:12:15 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdamOdonnel::Application.config.secret_key_base = 'f28332f2d678d815d86bf07b5fdadf6ccc87e194b47740f45efc95e7097676db0e9858729a7dc84c1eb2c62d84eada5d197b3fa82ddc4fdb4e46dfe322262bf4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AdamOdonnel::Application.config.session_store :cookie_store, key: '_adam_odonnel_session'
